e00909086f67035fcdd41a594826a54b08183265
stepantzov <stepantzov@gmail.com>
2017-07-17 13:25:02 +0200

    first version of  string reverse

---
+++ src/checking/ShowProperties.java
@@ -0,0 +1,13 @@
+package checking;
+
+/**
+ * Created by Ivan_Stepantsov on 7/12/2017.
+ */
+public class ShowProperties {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(
+                System.getProperty("java.library.path"));
+    }
+} ///:~
---
+++ src/training/string_reverse/StringReverseMain.java
@@ -0,0 +1,19 @@
+package training.string_reverse;
+
+import java.util.Scanner;
+
+/**
+ * Created by Ivan_Stepantsov on 7/16/2017.
+ */
+public class StringReverseMain {
+    public static void main(String[] args) {
+        String inputString;
+        System.out.println("Please enter your string to reverse:");
+        Scanner stringToReverse = new Scanner(System.in);
+        inputString = stringToReverse.nextLine();
+        char [] singleCharacter = inputString.toCharArray(); //basic difference between post and prefix [] - ?
+
+        for (int i = )
+
+    }
+}
