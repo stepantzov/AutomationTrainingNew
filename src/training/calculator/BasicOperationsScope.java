1cc4585578798bdb76d2169523947174d4ddb144
stepantzov <stepantzov@gmail.com>
2017-07-17 11:53:59 +0200

    Done with some refactoring: made classes separated; rearranged boolean flags; break added. made default access modifiers (package-private).

---
+++ src/training/calculator/BasicOperationsScope.java
@@ -0,0 +1,31 @@
+package training.calculator;
+
+/**
+ * Created by Ivan_Stepantsov on 7/16/2017.
+ */
+ class BasicOperationsScope {
+    int addOperationCalc(int operandOne, int operandTwo) {
+        int addOperationResult = operandOne + operandTwo;
+        return addOperationResult;
+    }
+
+    int subtractionOperationCalc(int operandOne, int operandTwo) {
+        int addOperationResult = operandOne - operandTwo;
+        return addOperationResult;
+    }
+
+    int multiplicationOperationCalc(int operandOne, int operandTwo) {
+        int addOperationResult = operandOne * operandTwo;
+        return addOperationResult;
+    }
+
+    int divisionOperationCalc(int operandOne, int operandTwo) {
+        int addOperationResult = operandOne / operandTwo;
+        return addOperationResult;
+    }
+
+    int reminderOperationCalc(int operandOne, int operandTwo) {
+        int addOperationResult = operandOne % operandTwo;
+        return addOperationResult;
+    }
+}
