467b6d0c9d63a3e22c67f008c37ddfafbec1598c
stepantzov <stepantzov@gmail.com>
2017-07-21 16:00:05 +0200

    second version after refactoring

--- src/training/numbers/palindrome/NumbersPalindromeLogic.java
+++ src/training/numbers/palindrome/NumbersPalindromeLogic.java
@@ -1,25 +1,27 @@
-package training.numbers_palindrome;
+package training.numbers.palindrome;
 
 /**
  * Created by Ivan_Stepantsov on 7/16/2017.
  */
 class NumbersPalindromeLogic {
-    boolean numbersPalindromeLogic(int number) {
+    boolean getPalindromeFlag(int number) {
         int n;
         int digit;
         int reverse = 0;
         n = number;
 
+        boolean flag = false;
         do {
             digit = number % 10;
             reverse = (reverse * 10) + digit;
             number = number / 10;
-        } while (number != 0);
+        }
+        while (number != 0);
 
         if (n == reverse) {
-            return true;
-        } else {
-            return false;
+            flag = true;
+            return flag;
         }
+        return flag;
     }
 }
