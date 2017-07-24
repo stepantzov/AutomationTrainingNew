ea2db87c9beec933ab6b60bb95024cb7e8448814
stepantzov <stepantzov@gmail.com>
2017-07-24 18:44:43 +0200

    next version of SelectionSort. added methods. rethinked of main.

--- src/training/arraysort/ArraySelectionSortMain.java
+++ src/training/arraysort/ArraySelectionSortMain.java
@@ -4,13 +4,16 @@ package training.arraysort;
  * Created by Ivan_Stepantsov on 7/21/2017.
  */
 public class ArraySelectionSortMain {
-    public static void main(String[] args) {
-        int[] myArray = {4, 5, 3, 6, 7, 4, 3, 2, 6, 7, 6, 3, -3, -4, -6, 3};
-        int[] sortedArray = ArraySelectionSort.doArraySelectionSort(myArray);
-
+    static void doSort(int[] array) {
+        int[] sortedArray = ArraySelectionSort.doArraySelectionSort(array);
         for (int i : sortedArray) {
             System.out.format("%d  ", i);
         }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {4, 5, 3, 6, 7, 4, 3, 2, 6, 7, 6, 3, -3, -4, -6, 3};
+        ArraySelectionSortMain.doSort(myArray);
         System.out.println("\nMin element = " + ArraySelectionSort.returnMinElement(myArray));
         System.out.println("Max element = " + ArraySelectionSort.returnMaxElement(myArray));
     }
