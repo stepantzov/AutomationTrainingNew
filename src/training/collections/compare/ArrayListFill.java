c45990cc4e1a592ab0a1633cb8cc3dceac9ffecc
stepantzov <stepantzov@gmail.com>
2017-08-30 14:00:29 +0200

    third verion of collection's some Clases refactoring was performed; Collection Obejct type included.

--- src/training/collections/compare/ArrayListFill.java
+++ src/training/collections/compare/ArrayListFill.java
@@ -1,13 +1,15 @@
 package training.collections.compare;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.ArrayList;
 
 /**
  * Created by Ivan_Stepantsov on 8/28/2017.
  */
-class ArrayListFill {
+class ArrayListFill  {
     static ArrayList<Integer> doArrayListFill(int elementsArray[]) {
-        ArrayList<Integer> myArrayList = new ArrayList<>();
+        ArrayList<Integer> myArrayList = new ArrayList<Integer>();
         for (int i : elementsArray) myArrayList.add(i);
         return myArrayList;
     }
