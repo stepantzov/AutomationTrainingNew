77793f061ab57b62a490b27c34558088002da3ff
stepantzov <stepantzov@gmail.com>
2017-08-31 10:31:21 +0200

    fourth version of collection's some Class refactoring was performed; non Object type currently used. collection is used as a instansiate parameter.

--- src/training/collections/compare/LinkedListFill.java
+++ src/training/collections/compare/LinkedListFill.java
@@ -6,12 +6,8 @@ import java.util.LinkedList;
  * Created by Ivan_Stepantsov on 8/28/2017.
  */
 class LinkedListFill {
-    static LinkedList <Integer> doLinkedListFill(int elementsArray[]) {
-        //LinkedList <Integer> myLinkedList = new LinkedList<Integer>();
-        LinkedList newLinkedList = new LinkedList (CollectionFill.doBasicCollectionCreation(elementsArray));
-       /* for (int j = 0; j < elementsArray.length; j++) {
-            myLinkedList.add(j, elementsArray[j]);
-        }*/
+    static LinkedList<Integer> doLinkedListFill(int elementsArray[]) {
+        LinkedList newLinkedList = new LinkedList(CollectionFill.doBasicCollectionCreation(elementsArray));
         return newLinkedList;
     }
 
--- src/training/collections/compare/Main.java
+++ src/training/collections/compare/Main.java
@@ -13,14 +13,14 @@ public class Main {
         Scanner elementsQuantityScanner = new Scanner(System.in);
         int elementsQuantity = elementsQuantityScanner.nextInt();
         int elementsArray[] = new int[elementsQuantity];
-        Collection <Integer> myTestCollection = new ArrayList<Integer>();
+        Collection<Integer> myTestCollection = new ArrayList<Integer>();
         for (int i = 0; i < elementsArray.length; i++) {
             elementsArray[i] = (int) Math.round((Math.random() * 100));
         }
         System.out.println("Map structure:\n" + HashMapFill.doHashMapFill(elementsArray));
         System.out.println("Collection structure:\n" + CollectionFill.doBasicCollectionCreation(elementsArray));
         MidElementFinding.getBasicCollectionsMidElement(CollectionFill.doBasicCollectionCreation(elementsArray));
-        MidElementFinding.getArrayListMidElement(ArrayListFill.doArrayListFill(elementsArray));
+        MidElementFinding.getLinkedListMidElement(LinkedListFill.doLinkedListFill(elementsArray));
         MidElementFinding.getLinkedListMidElement(LinkedListFill.doLinkedListFill(elementsArray));
         MidElementFinding.getHashMapMidElement(HashMapFill.doHashMapFill(elementsArray));
     }
