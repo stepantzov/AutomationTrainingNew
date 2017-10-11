6d6cd7080cece68a45f88b9d56e3df522e003be7
stepantzov <stepantzov@gmail.com>
2017-10-11 15:42:00 +0200

    JUnit Tests for Calculator feature, first version; -used annotations; -test suits; -test factory.

---
+++ src/training/calculator/test/JunitTestSuite.java
@@ -0,0 +1,12 @@
+package training.calculator.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestBasicCalculatorOperations.class, TestBasicCalculatorOperationsTestFactory.class,
+        TestNumberCheck.class})
+
+public class JunitTestSuite {
+
+}
